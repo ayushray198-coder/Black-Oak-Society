720774022c60b677fa108c279b18c4a6baf5365d
Ayush Kumar Rai <ayushray198@gmail.com>
2026-07-30 10:37:38 +0530

    fix(auth): update cookie sameSite for production login

--- backend/utils/cookiesOptions.js
+++ backend/utils/cookiesOptions.js
@@ -1,7 +1,7 @@
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
 };
 
 export default cookieOptions;
